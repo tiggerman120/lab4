ae4ecdbe5fd0ec32a4c71cd7c0dc8105b9206a05
Garrett Cintron <grrttcintron@gmail.com>
2020-08-19 16:55:49 -0700

    added javascript and hated life

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ var userName = prompt('Please Enter your Name');
 
 alert('Hello ' + userName);
 
-if (userName === 'Garrett'){
-    document.write('<h2>' + 'Your name is toby,' + userName + '</h2>'')}
-else {document.write('<h2>' + userName + 'Leave now or else' + '</h2>')}
+if (userName === 'Garrett')
+{document.write('<h2>' + 'Your name is toby, ' + userName + '</h2>')}
+else {document.write('<h2>' + userName + ' Leave now or else' + '</h2>')}
 
 
-document.write('<h2>' + "Welcome " + userName + '</h2>')
+var chickenCount = prompt('Please tell us how much chicken you eat');
+if (chickenCount > 0){
+    alert('eat more chickens!');
+}else {
+    document.write('<p> eat more chicken!</p>')
+}
+
+var ownership = prompt('Do you want to be a chicken farmer?')
+alert('You should buy our chickens and be a birbma!');

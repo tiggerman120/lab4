57dc3ef6d87289eea9789cac3425a3c91102cc28
Garrett Cintron <grrttcintron@gmail.com>
2020-08-20 12:29:32 -0700

    the fuck if i know this shit makes zero sense and teacher wont fucking explain it

--- app.js
+++ app.js
@@ -4,17 +4,17 @@ var userName = prompt('Please Enter your Name');
 
 alert('Hello ' + userName);
 
+
 if (userName === 'Garrett')
 {document.write('<h2>' + 'Your name is toby, ' + userName + '</h2>')}
 else {document.write('<h2>' + userName + ' Leave now or else' + '</h2>')}
 
 
 var chickenCount = prompt('Please tell us how much chicken you eat');
-if (chickenCount = none){
-    alert('eat more chickens!');
-}else {
-    document.write('<p> eat more chicken!</p>')
-}
+if (chickenCount = none)
+    {alert('eat more chickens!');}
+
+    else {document.write('<p> eat more chicken!</p>')}
 
 var ownership = prompt('Do you want to be a chicken farmer?')
 alert('You should buy our chickens and be a birbma!');
